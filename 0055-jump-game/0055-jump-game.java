a7a86f5ee8dfb3673a440dd6de08b703b02637d9
Angelina Mathew <angelinamathew707@gmail.com>
2024-12-02 14:36:06 -0500

    Time: 1 ms (100%), Space: 45.9 MB (16.83%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int finalPosition = nums.length - 1;
+        for(int i= nums.length-2; i>=0; i--){
+            if(i + nums[i] >= finalPosition){
+                finalPosition = i;
+            }
+        }
+        return finalPosition == 0;
+    }
+}
